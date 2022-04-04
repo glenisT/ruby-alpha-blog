7ac3a60e63dfb6b03c52a51cb9f9d1cd9008daf3
glenisT <glentivari@gmail.com>
2022-04-04 15:45:50 +0200

    Add article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-    def show
+    def show    #shows singular articles and their attributes
         @article = Article.find(params[:id])  #instance variable (so it's available to the view) that saves the Article pbject info and shows it through picking the corresponding id from the params hash
+    end
+
+    def index   #shows list of articles
+        @articles = Article.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"   #root page is received from pages_controller 'home' method/action 
   get "about", to: "pages#about"    #for other pages
-  resources :articles, only: [:show] #gets all the paths related to the Article CRUD (in this case only SHOW)
+  resources :articles, only: [:show, :index] #gets all the paths related to the Article CRUD (in this case only SHOW)
 end

1460a209d651edd17675fca47b91a2ef05d3f356
glenisT <glentivari@gmail.com>
2022-04-05 14:15:41 +0200

    Add DELETE and index page links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"   #root page is received from pages_controller 'home' method/action 
   get "about", to: "pages#about"    #for other pages
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update] #gets all the paths related to the Article CRUD (check 'rails routes --expanded' command in terminal)
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] gets only the specified paths related to the Article CRUD (check 'rails routes --expanded' command in terminal)
+  resources :articles
 end

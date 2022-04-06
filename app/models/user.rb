fa18aa03525e765e4c317cbc3370fe329f71482b
glenisT <glentivari@gmail.com>
2022-04-06 17:55:01 +0200

    Add users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-    
+    validates :username, 
+                presence: true, 
+                uniqueness: {case_sensitive: false}, #uniquieness: makes it so that there cannot be duplicate values for these attributes in the DB
+                length: {minimum: 3, maximum: 25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  #REGular EXpression constant to validate email format. Can be tested at rubular.com
+    validates :email, 
+                presence: true, 
+                uniqueness: {case_sensitive: false}, 
+                length: {maximum: 105}, 
+                format: {with: VALID_EMAIL_REGEX}
 end

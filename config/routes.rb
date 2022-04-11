01c7392c3cc2769c6dcd33c9398bcc3feabaac0d
glenisT <glentivari@gmail.com>
2022-04-11 18:20:01 +0200

    Add users sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"    #for other pages
   resources :articles
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] gets only the specified paths related to the Article CRUD (check 'rails routes --expanded' command in terminal)
+  get "signup", to: "users#new"
 end

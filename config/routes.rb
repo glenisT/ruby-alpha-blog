1ef8ed10f6ac521d28ad737eade0eefeb512ef79
glenisT <glentivari@gmail.com>
2022-04-13 18:07:05 +0200

    Add log in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  root "pages#home"   #root page is received from pages_controller 'home' method/action 
-  get "about", to: "pages#about"    #for other pages
+  root "pages#home" #root page is received from pages_controller 'home' method/action 
+  get "about", to: "pages#about" #for other pages
+
   resources :articles
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] gets only the specified paths related to the Article CRUD (check 'rails routes --expanded' command in terminal)
+
   get "signup", to: "users#new"
-  #post "users", to: "users#create" | to create path specific to POST method
   resources :users, except: [:new] #except new because it is already defined above
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#submit'
+  delete 'logout', to: 'sessions#destroy'
 end

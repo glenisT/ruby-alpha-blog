17504abbbc9bac79a46014b16fe0518b672c5ef1
glenisT <glentivari@gmail.com>
2022-04-15 09:56:40 +0200

    Add further restrictions based on logged-in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    #so that these helper methods are available to all controllers
+    helper_method :current_user, :logged_in?
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user #turns current_user method into a boolean
+    end
+
 end
